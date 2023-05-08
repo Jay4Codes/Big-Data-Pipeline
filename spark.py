b782e2a9cbddb783e286f362b0b03af7a6a17723
jay4codes <jay4codes@gmail.com>
2023-05-09 04:09:17 +0530

    spark updated

--- spark.py
+++ spark.py
@@ -1,24 +1,50 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, from_unixtime
 
-spark = SparkSession.builder.enableHiveSupport().getOrCreate()
+# Create a SparkSession
+spark = SparkSession.builder.appName("Process Plant Data").getOrCreate()
 
 print('Starting spark Session')
-spark.range(100).write.mode("overwrite").parquet(
-    "gs://blissful-hash-312105/random_numbers")
 
-# Load the CSV file into a DataFrame
-df = spark.read.format("csv") \
+site_df = spark.read.format("csv") \
     .option("header", "true") \
     .option("inferSchema", "true") \
-    .load("./site_data.csv")
+    .load("gs://bde-git/bde_data/site_metadata.csv")
 
-# Print the schema of the DataFrame
-df.printSchema()
+print('Loaded site metadata')
 
-# Show the first 20 rows of the DataFrame
-df.show(20)
+df = spark.read.format("csv").option("header", "true").option(
+    "inferSchema", "true").load("gs://bde-git/bde_data/plant_data/ABORIGINAL-PICULET.csv")
 
-print('complete')
+print('Loaded plant data')
+
+# Fill null values with 0
+df = df.fillna(0)
+
+print('Filled null values')
+
+# Rename column "Unnamed: 0" to "ID"
+df = df.withColumnRenamed("Unnamed: 0", "ID")
+
+print('Renamed column')
+
+# Convert "DATE_TIME" column to timestamp and split into "DATE" and "TIME" columns
+# df = df.withColumn("DATE_TIME", from_unixtime(col("DATE_TIME"))).withColumn("DATE", col(
+#     "DATE_TIME").cast("date")).withColumn("TIME", col("DATE_TIME").cast("time")).drop("DATE_TIME")
+
+# Calculate thermal efficiency for each plant using the fuel LHV value from site_df
+for i in range(1, int((len(df.columns) - 3) / 4) + 1):
+    for j in range(45):
+        df = df.withColumn("THERMAL_EFF", col(
+            f"POWER_{i}") / (col(f"FUEL_FLOW_{i}") * 21503.417436) * 100)
+
+print('Calculated thermal efficiency')
+
+# Write the processed DataFrame to CSV
+df.write.format("csv").option("header", "true").mode(
+    "overwrite").option("delimiter", ",").save("gs://bde-git/analysed_data/ABORIGINAL-PICULET.csv")
+
+print('Finished processing files')
 
 # Stop the SparkSession
 spark.stop()
